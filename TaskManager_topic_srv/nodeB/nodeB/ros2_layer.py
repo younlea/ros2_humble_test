1c6609f7c6e3aa77734a971a1facd65748570592
Younlea.kim <younlea.kim@gmail.com>
2024-08-22 14:35:23 +0900

    Update ros2_layer.py

--- TaskManager_topic_srv/nodeB/nodeB/ros2_layer.py
+++ TaskManager_topic_srv/nodeB/nodeB/ros2_layer.py
@@ -1,28 +1,30 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from message.msg import DataAB  # Use DataAB message type
 from message.srv import SrvBA
 
+# Import the necessary classes
 from nodeB.intermediate_layer import NodeBIntermediate
 from nodeB.process_layer import NodeBProcess
 
 class NodeBRos2(Node):
     def __init__(self):
         super().__init__('node_b')
-        self.subscription = self.create_subscription(Int32, 'data_ab', self.listener_callback, 10)
+        self.subscription = self.create_subscription(DataAB, 'data_ab', self.listener_callback, 10)  # Use DataAB message type
         self.srv_client = self.create_client(SrvBA, 'srv_ba')
         self.intermediate_layer = NodeBIntermediate(NodeBProcess())
 
     def listener_callback(self, msg):
-        print(f"Received: {msg.data}")
-        sum_ab = self.intermediate_layer.handle_data_ab(msg.data)
+        print(f"Received DataAB: {msg.data_ab}")
+        sum_ab = self.intermediate_layer.handle_data_ab(msg.data_ab)
         if sum_ab is not None:
             self.call_srv_ba(sum_ab)
 
     def call_srv_ba(self, data):
         request = SrvBA.Request()
         request.data_ba = data
-        self.srv_client.call_async(request).add_done_callback(self.handle_srv_ba_response)
+        future = self.srv_client.call_async(request)
+        future.add_done_callback(self.handle_srv_ba_response)
 
     def handle_srv_ba_response(self, future):
         response = future.result()
